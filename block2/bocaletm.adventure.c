fbb3253d79c0c10d22812f0de21b5d8e30a664a2
Mario Termux <bocaletm@oregonstate.edu>
2019-02-09 15:39:43 -0600

    trying strtok

--- block2/bocaletm.adventure.c
+++ block2/bocaletm.adventure.c
@@ -17,17 +17,10 @@
 #define CREATED_ROOMS 7
 const char* const ROOMS[] = {"lobby", "cafe", "bank", "restroom", "office", "garage", "lounge", "pool", "elevator", "helipad"};
 
-
-typedef struct  {
-  char name[10];
-  char type[10];
-  int** connections;
-} Room;
-
 typedef struct {
   int steps;
-  int idx;
-  Room** rooms;
+  int room_idx;
+  char** rooms;
   char** path;
 } Game; 
 
@@ -35,7 +28,7 @@ typedef struct {
  * checkMem(): checks if malloc
  * succeeded
  * ******************/
-checkMem(char* ptr) {
+void checkMem(char* ptr) {
   if (ptr == 0) {
       printf("Malloc error.");
       exit(1);
@@ -73,14 +66,14 @@ void getDir(char* dirName) {
   }
   closedir(dirToCheck);
   /*copy newestDirName to parameter*/
-  memset(dirName,'\0',sizeof(dirName));
+  memset(dirName,'\0',256 * sizeof(char));
   sprintf(dirName,"./%s",newestDirName);
 }
 /*********************
  * findStart(): finds the room
  * to start the game. Edits path to file
  *********************/
-void findStart(char* filepath) {
+void findStartEnd(char* start_filepath, char* end_filepath) {
   /*find newest directory*/
   char* dirName = 0;
   dirName = malloc(256 * sizeof(char));
@@ -92,7 +85,6 @@ void findStart(char* filepath) {
   char readBuffer[256];
   DIR* dirToCheck;
   struct dirent *fileInDir;
-  struct stat dirAttributes;
   int file_descriptor = 0;
   int nread = 0;
   /*check present directory*/
@@ -114,7 +106,9 @@ void findStart(char* filepath) {
       /*read the last chars in last line*/
       nread = read(file_descriptor,readBuffer,sizeof(readBuffer));
       if (strstr(readBuffer,"START")) {
-        strcpy(filepath,tempFilepath);
+        strcpy(start_filepath,tempFilepath);
+      } else if (strstr(readBuffer,"END")) {
+        strcpy(end_filepath,tempFilepath);
       }
     }
   } else {
@@ -125,17 +119,53 @@ void findStart(char* filepath) {
 }
 
 /*********************
- * readRoom(): reads the room
+ * getNextRoom(): reads the room
  *********************/
-void readRoom(char* filepath, struct Room* room) {
-
+void getNextRoom(char* filepath, Game* newGame) {
+  
 }
 
 /*********************
  * printRoom(): prints the room
  *********************/
-void printRoom(struct Room* room) {
+void printRoom(char* selection_filepath) {
+  FILE* file_ptr = 0;
+  /*open the file*/
+  file_ptr = fopen(selection_filepath,"r");
+  if (file_ptr == 0) {
+    printf("Could not open file %s\n",selection_filepath);
+  }
+  int nameLength = 6;
+  /*read the name of the room*/
+  char name[nameLength];
+  memset(name,'\0',sizeof(name));
+  
+  /*move to start of name*/
+  fseek(file_ptr,11,SEEK_SET);
+  /*read rest of line*/
+  fgets(name,nameLength,file_ptr);
+  printf("CURRENT LOCATION: %s\n",name);
 
+  /*find out how many connections to read*/
+  int connections = 3;
+  /*move to next connection*/
+  int buffSize = 20;
+  int i;
+  char data[buffSize];
+  char* token = 0;
+  char full_token[20];
+  printf("POSSIBLE CONNECTIONS: ");
+  memset(data,'\0',sizeof(data));
+  while (fgets(data,buffSize,file_ptr)) {
+    for (i = 1; i <= connections; i++) {
+      memset(full_token,'\0',sizeof(full_token));
+      sprintf(full_token,"CONNECTION %d",i);
+      token = strtok(data,full_token);
+      printf("%s",token);
+    }
+  }
+
+  /*read the connections*/
 }
 
 /*********************
@@ -148,7 +178,7 @@ void getInput(char* input) {
 /*********************
  * endGame(): displays end of game
  * *******************/
-void endGame(struct Game* game) {
+void endGame(Game* game) {
 
 }
 
@@ -157,17 +187,45 @@ void endGame(struct Game* game) {
  * *******************/
 int main() {
 
-  Game newGame;
-  newGame.steps = 0;
-  newGame.idx = 0;
-  newGame.path = malloc(CREATED_ROOMS * sizeof(char*));
-  checkMem(newGame.path);
+  Game* newGame = 0;
+  newGame = malloc(sizeof(Game));
+  if (newGame == 0) {
+    printf("Malloc error creating new Game\n");
+    exit(1);
+  }
+  newGame->steps = 0;
+  newGame->room_idx = 0;
+  newGame->path = malloc(CREATED_ROOMS * sizeof(char*));
+  checkMem(*newGame->path);
 
-  char* filepath = 0;
-  filepath = malloc(50 * sizeof(char));
-  checkMem(filepath);
-  findStart(filepath);
+  char* start_filepath = 0;
+  start_filepath = malloc(50 * sizeof(char));
+  checkMem(start_filepath);
+  
+  char* end_filepath = 0;
+  end_filepath = malloc(50 * sizeof(char));
+  checkMem(end_filepath);
+  
+  char* selection_filepath = 0;
+  selection_filepath = malloc(50 * sizeof(char));
+  checkMem(selection_filepath);
 
+  findStartEnd(start_filepath,end_filepath);
 
+  memset(selection_filepath,'\0',50 * sizeof(char));
+  strcpy(selection_filepath,start_filepath);
+  printf("start: %s select: %s\n",start_filepath,selection_filepath);
+  printRoom(selection_filepath);
+  int end = 0;
+ /* while (end == 0) {
+    printRoom(selection_filepath);
+    memset(selection_filepath,'\0',50 * sizeof(char));
+    getNextRoom(selection_filepath,newGame);
+    if (strstr(selection_filepath,end_filepath)) {
+      end=1;
+      break;
+    }
+  }
+  endGame(newGame);*/
 }
 
