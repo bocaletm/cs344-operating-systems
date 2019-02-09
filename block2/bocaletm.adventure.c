a2e8363ab7eca772256440d789ceb0c1ab049424
Mario Termux <bocaletm@oregonstate.edu>
2019-02-08 23:30:36 -0600

    starting adv

--- block2/bocaletm.adventure.c
+++ block2/bocaletm.adventure.c
@@ -6,23 +6,55 @@
  *********************/
 #include <sys/stat.h>
 #include <stdio.h>
+#include <dirent.h>
+#include <sys/types.h>
 #include <stdlib.h>
 #include <string.h>
 #include <time.h>
 #include <unistd.h>
 #include <fcntl.h>
 
+#define CREATED_ROOMS 7
 const char* const ROOMS[] = {"lobby", "cafe", "bank", "restroom", "office", "garage", "lounge", "pool", "elevator", "helipad"};
 
-struct Room {
+
+typedef struct  {
   char name[10];
   char type[10];
   int** connections;
-};
+} Room;
 
-struct Game {
+typedef struct {
   int steps;
+  int idx;
   char** path;
+} Game; 
+
+/********************
+ * checkMem(): checks if malloc
+ * succeeded
+ * ******************/
+checkMem(char* ptr,char** function) {
+  if (ptr == 0) {
+      printf("Malloc error in %s\n",function);
+      exit(1);
+  }
+}
+
+/*********************
+ * getDir(): finds newest directory
+ * *******************/
+void getDir(char* dirName) {
+  DIR* dirPtr = 0;
+  struct dirent* entityPtr = 0;
+  dirPtr = opendir("./");
+  if (dirPtr != NULL) {
+    while(entityPtr == readdir(dirPtr)){
+      puts(entityPtr->d_name);
+      printf("%s\n",entityPtr->d_name);
+    }
+    (void) closedir(dirPtr);
+  }
 }
 
 /*********************
@@ -30,7 +62,23 @@ struct Game {
  * to start the game. Edits path to file
  *********************/
 void findStart(char* filepath) {
+  /*find newest directory*/
+  char* dirName = 0;
+  dirName = malloc(25 * sizeof(char));
+  checkMem(dirName,"findStart()");
+  getDir(dirName);
 
+  /*set filepath of start room*/
+
+  /*get first room name*/
+
+  /*append room name to directory*/
+
+  /*open file*/
+
+  /*check file type*/
+
+  /*set filepath*/
 }
 
 /*********************
@@ -66,9 +114,16 @@ void endGame(struct Game* game) {
  * *******************/
 int main() {
 
+  Game newGame;
+  newGame.steps = 0;
+  newGame.idx = 0;
+  newGame.path = malloc(CREATED_ROOMS * sizeof(char*));
+  checkMem(newGame.path,"main()");
 
+  char* filepath = 0;
+  filepath = malloc(50 * sizeof(char));
+  checkMem(filepath,"main()");
 
-
-
+  findStart(filepath);
 }
 
