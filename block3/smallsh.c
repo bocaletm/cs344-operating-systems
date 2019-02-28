ffbb443677836ebcc55a4d515ca6ace6a95c7e62
bocalettirocco <bocalettirocco@gmail.com>
2019-02-27 19:30:53 -0600

    adding signal handling ctrl z

--- block3/smallsh.c
+++ block3/smallsh.c
@@ -10,13 +10,77 @@
 #include <sys/types.h>
 #include <stdlib.h>
 #include <string.h>
+#include <unistd.h>
+#include <signal.h>
 
+int toggleBackgroundProc = 1;
+const int MAX_CHARS = 2048;
 
+/*********************
+ * sigstpHandler(): handles ctrl-z 
+ *********************/
+void sigstpHandler(int signo) {
+  char* message = "\nctrl-z switching states...";
+  write(STDOUT_FILENO, message, 28);
+  toggleBackgroundProc = 1 - toggleBackgroundProc;
+  sleep(1);
+}
+
+/*********************
+ * execute(): performs I/O redirection and exec 
+ *********************/
+void execute(command) {
+    exec(command);
+}
+/*********************
+ * exitGracefully(): kills any processes and jobs
+ * and exits zero 
+ *********************/
+void exitGracefully() {
+  exit(0);
+}
+
+/*********************
+ * changeDir(): cd command 
+ *********************/
+void changeDir(char* dir) {
+  
+}
+
+/*********************
+ * processCmd(): processes string into commands 
+ *********************/
+void processCmd(char* rawCmd) {
+  char* cmd;
+  int lastChar = -1;
+  //get first string
+  for (i = 0; i < MAX_CHARS; i++) {
+    if (command[i] == ' ') {
+        break;
+    }
+    lastChar++;
+  } 
+  cmd = malloc((lastChar + 1) * sizeof(char));
+  memset(cmd,'\0',sizeof(cmd));
+  for (i = 0; i <= lastChar; i++) {
+      cmd[i] = rawCmd[i];
+  }
+
+  //check first string to see if it's native cmd
+  if (strcmp(cmd,"exit") == 0) {
+    exitGracefully();
+  } else if (strcmp(cmd,"cd") == 0) {
+    changeDir(rawCmd);
+  } else if (strcmp(cmd,"status") == 0) {
+    getStatus();
+  }
+  //run anything non-native
+  execute(rawCmd);
+} 
 /*********************
  * getInput(): gets user selection 
  *********************/
 char* getInput() {
-  const int MAX_CHARS = 2048;
   int valid = 0;
   int charsEntered = -5;
   int idx = 0;
@@ -51,8 +115,21 @@ char* getInput() {
  * main()
  * *******************/
 int main() {
-  char* command = 0;
-  command = getInput(command);
-  printf("\t%s\n",command);
-  free(command);
+  //ignore ctrl+c
+  signal(SIGINT,SIG_IGN);
+  //handle ctrl+z
+  struct sigaction sa_sigtstp = {0};
+  sa_sigtstp.sa_handler = sigstpHandler; 
+  sigfillset(&sa_sigtstp.sa_mask);
+  sa_sigtstp.sa_flags = 0;
+  //endless loop; exit will terminate shell from
+  //processCmd function
+  while (1 == 1) {
+    char* command = 0;
+    command = getInput(command);
+    processCmd(command); 
+    if (command != 0) {
+      free(command);
+    }
+  }
 }
